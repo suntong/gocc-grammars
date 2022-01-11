21a2c19a10bac54d025e819357c1e2d1c67ff619
Tong Sun <suntong@cpan.org>
2022-01-11 12:09:26 -0500

    - [+] _test add TestPass() & TestFail()

--- bnf/bnf_test.go
+++ bnf/bnf_test.go
@@ -11,6 +11,38 @@ import (
 	"github.com/suntong/gocc-grammars/bnf/parser"
 )
 
+var testData = []string{
+	// "// line comment",
+	// "/* block comment */",
+	"floatnumber ::=  pointfloat | exponentfloat ;",
+	`<expr> ::= <term> "+" <expr>
+        |  <term>
+        ;`,
+}
+
+func TestPass(t *testing.T) {
+	p := parser.NewParser()
+	for _, ts := range testData {
+		fmt.Printf("  Testing: %s\n", ts)
+		s := lexer.NewLexer([]byte(ts))
+		r, err := p.Parse(s)
+		if err != nil {
+			t.Error(err)
+		}
+		_ = r
+		//fmt.Printf("output: %s\n", r)
+	}
+}
+
+func TestFail(t *testing.T) {
+	_, err := test([]byte("a = b"))
+	if err == nil {
+		t.Fatal("unexpected parse error")
+	} else {
+		fmt.Printf("  Parsing failed as expected: %v\n", err)
+	}
+}
+
 func TestFiles(t *testing.T) {
 	files := []string{}
 	err := filepath.Walk("test",
@@ -40,3 +72,38 @@ func TestFiles(t *testing.T) {
 		}
 	}
 }
+
+func test(src []byte) (astree interface{}, err error) {
+	fmt.Printf("  Testing: %s\n", src)
+	s := lexer.NewLexer(src)
+	p := parser.NewParser()
+	a, err := p.Parse(s)
+	if err == nil {
+		astree = a
+	}
+	return
+}
+
+/*
+
+$ go test -v .
+=== RUN   TestPass
+  Testing: floatnumber ::=  pointfloat | exponentfloat ;
+  Testing: <expr> ::= <term> "+" <expr>
+        |  <term>
+        ;
+--- PASS: TestPass (0.00s)
+=== RUN   TestFail
+  Testing: a = b ;
+  Parsing failed as expected: 1:3: error: expected assign; got: unknown/invalid token "="
+--- PASS: TestFail (0.00s)
+=== RUN   TestFiles
+  Testing file: test/calc.bnf
+  Testing file: test/float.bnf
+  Testing file: test/regexp-plg.bnf
+  Testing file: test/shell.bnf
+--- PASS: TestFiles (0.00s)
+PASS
+ok      github.com/suntong/gocc-grammars/bnf    0.001s
+
+*/
